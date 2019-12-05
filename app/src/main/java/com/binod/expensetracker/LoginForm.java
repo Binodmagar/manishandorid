fe8de6b010b9451a24d6500534ecfd290989c5ff
Binodmagar <ranabinod615@gmail.com>
2019-12-05 18:03:49 +0545

    register design finished

--- app/src/main/java/com/binod/expensetracker/LoginForm.java
+++ app/src/main/java/com/binod/expensetracker/LoginForm.java
@@ -2,14 +2,94 @@ package com.binod.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.icu.util.UniversalTimeScale;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginForm extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginForm extends AppCompatActivity implements View.OnClickListener {
+
+    private static View view;
+    public static EditText etEmail, etPassword;
+    private static Button btnLogin;
+    private static TextView tvCreateAccount;
+    private static Animation animation;
+
+    public void Login_Fragment(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
 //        getSupportActionBar().setTitle("test");
+
+        //binding
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvCreateAccount = findViewById(R.id.tvCreateAccount);
+
+        //listener
+        btnLogin.setOnClickListener(this);
+        tvCreateAccount.setOnClickListener(this);
+
+//      animation = AnimationUtils.loadAnimation() ;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnLogin:
+                checkValidation();
+                break;
+
+
+            case R.id.tvCreateAccount:
+                Intent intent = new Intent(LoginForm.this, RegisterActivity.class);
+                startActivity(intent);
+
+
+        }
+    }
+
+    private  void checkValidation(){
+        //get email and password
+        String getEmail = etEmail.getText().toString();
+        String getPassword = etPassword.getText().toString();
+
+        if(TextUtils.isEmpty(etEmail.getText()) || etEmail.length() == 0){
+         Toast.makeText(getApplicationContext(), "Enter your email", Toast.LENGTH_SHORT).show();
+
+            return;
+        }else if(TextUtils.isEmpty(etPassword.getText())){
+            etPassword.setError("Plase enter your password");
+            return;
+        }
+
+
+        //check pattern for email
+//        Pattern p = Pattern.compile();
+//        Matcher m = p.matcher(getEmail);
+
+        //check if email and password is empty or not
+        if(getEmail.equals("") || getEmail.length() == 0
+        || getPassword.equals("") || getPassword.length() == 0){
+            Toast.makeText(getApplicationContext(), "Enter Both credentials.", Toast.LENGTH_SHORT).show();
+        }
+
+//        else if(!m.find())
+
     }
 }
--- app/src/main/java/com/binod/expensetracker/RegisterActivity.java
+++ app/src/main/java/com/binod/expensetracker/RegisterActivity.java
@@ -2,13 +2,48 @@ package com.binod.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static Button btnSignUp;
+    private static TextView tvAlreadyUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //binding
+        btnSignUp = findViewById(R.id.btnSignUp);
+        tvAlreadyUser = findViewById(R.id.tvAlreadyUser);
+
+        //listener
+        btnSignUp.setOnClickListener(this);
+        tvAlreadyUser.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSignUp:
+                checkValidation();
+                break;
+
+            case R.id.tvAlreadyUser:
+                Intent intent = new Intent(RegisterActivity.this, LoginForm.class);
+                startActivity(intent);
+        }
+    }
+
+    private void checkValidation(){
+
     }
 }
