e4acc8091b8f29fa969040e6f91f6ff53836612a
Binodmagar <ranabinod615@gmail.com>
2020-02-02 00:19:57 +0545

    Unit testing of LoginBLLTest

--- app/src/test/java/com/binod/expensetracker/LoginBLLTest.java
+++ app/src/test/java/com/binod/expensetracker/LoginBLLTest.java
@@ -2,15 +2,21 @@ package com.binod.expensetracker;
 
 import com.binod.bll.LoginBLL;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class LoginBLLTest {
+    com.binod.bll.LoginBLL loginBLL ;
+    @Before
+    public void setUp(){
+        loginBLL = new com.binod.bll.LoginBLL();
+    }
 
     @Test
     public void testLogin(){
-        com.binod.bll.LoginBLL loginBLL = new com.binod.bll.LoginBLL();
+
         boolean result = loginBLL.checkUser("binod@gmail.com", "admin12345");
         assertEquals(true, result);
     }
