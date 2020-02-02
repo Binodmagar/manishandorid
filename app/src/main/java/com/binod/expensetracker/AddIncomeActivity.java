b523824d81c1c62dec5ec10600dde1a91c74630a
Binodmagar <ranabinod615@gmail.com>
2020-02-02 13:53:54 +0545

    change in design of add income and done binding and setting up adapter

--- app/src/main/java/com/binod/expensetracker/AddIncomeActivity.java
+++ app/src/main/java/com/binod/expensetracker/AddIncomeActivity.java
@@ -5,29 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class AddIncomeActivity extends AppCompatActivity {
 
-    Spinner spIncome;
+    Spinner spIncomeAI, spAccountAI;
+    EditText etNameAI,etAmountAI,etNoteAI;
+    Button btnSaveAI;
     TextView tvDateAI;
 
     public static final String Income[] = {
             "Salary",
             "Bonous",
     };
+
+    public static final String Account[] = {
+            "Cash",
+            "Cheque",
+            "Card"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_income);
+
+        //binding
+        etNameAI = findViewById(R.id.etNameAI);
+        etAmountAI = findViewById(R.id.etAmountAI);
+        etNoteAI = findViewById(R.id.etNoteAI);
         tvDateAI = findViewById(R.id.tvDateAI);
+        spIncomeAI = findViewById(R.id.spIncomeAI);
+        spAccountAI = findViewById(R.id.spAccountAI);
+        btnSaveAI = findViewById(R.id.btnSaveAI);
 
-        spIncome = findViewById(R.id.spIncome);
-
+        //setting up data in adapter
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,Income);
-        spIncome.setAdapter(arrayAdapter);
+        spIncomeAI.setAdapter(arrayAdapter);
+        ArrayAdapter arrayAdapter1 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Account);
+        spAccountAI.setAdapter(arrayAdapter1);
 
+        //for incomming data from intent
         Intent incommingIntent = getIntent();
         String date = incommingIntent.getStringExtra("currentDate");
         tvDateAI.setText(date);
